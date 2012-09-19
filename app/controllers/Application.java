ed877e30f7c09e1e5a536b9e0d9f8f41266c4953
techzhou <yzlkester@gmail.com>
2012-09-19 16:51:22 +0800

    主要框架功能

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,76 @@
 package controllers;
 
+import com.avaje.ebean.Ebean;
+import com.avaje.ebean.EbeanServer;
+import com.avaje.ebean.ExpressionList;
+import models.*;
+import org.codehaus.jackson.map.ObjectMapper;
 import play.*;
+import play.db.ebean.Model;
 import play.mvc.*;
 
 import views.html.*;
 
+import java.io.StringWriter;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
 public class Application extends Controller {
   
-  public static Result index() {
-    return ok(index.render("Your new application is ready."));
-  }
-  
+    public static Result index() {
+        return ok(index.render());
+    }
+
+    public static Result search(String query) {
+        String[] queryData = query.trim().split(" ");
+        ExpressionList<GeoData> expressionList = Ebean.getServer("lib").find(GeoData.class).where();
+        for(String aQuery : queryData){
+            aQuery = aQuery.trim();
+            if(aQuery.length() != 0)
+                expressionList = expressionList.contains("pkiia", aQuery);
+        }
+        Map<String, Set<GeoData>> departmentMap = new TreeMap<String, Set<GeoData>>();
+        for(GeoData data : expressionList.findSet()){
+            data.clean();
+            if(departmentMap.containsKey(data.department))
+                departmentMap.get(data.department).add(data);
+            else{
+                Set<GeoData> geoDataSet = new TreeSet<GeoData>();
+                geoDataSet.add(data);
+                departmentMap.put(data.department, geoDataSet);
+            }
+        }
+        StringWriter writer = new StringWriter();
+        ObjectMapper mapper = new ObjectMapper();
+        try{
+            mapper.writeValue(writer, departmentMap);
+        }catch (Exception e){
+            return internalServerError();
+        }
+        return ok(writer.toString());
+    }
+
+    public static Result getDetail(String id) {
+        Set<AttachDocument> attachDocuments = Ebean.getServer("lib").find(AttachDocument.class).where().like("BH", "%" + id + "%").findSet();
+        Set<AttachForm> attachForms = Ebean.getServer("lib").find(AttachForm.class).where().like("BH", "%" + id + "%").findSet();
+        Set<AttachPicture> attachPictures = Ebean.getServer("lib").find(AttachPicture.class).where().like("BH", "%" + id + "%").findSet();
+        Set<AttachElse> attachElses = Ebean.getServer("lib").find(AttachElse.class).where().like("BH", "%" + id + "%").findSet();
+        Set<OtherData> otherDatas = Ebean.getServer("lib").find(OtherData.class).where().like("BH", "%" + id + "%").findSet();
+        Map<String, Set<?>> detailMap = new TreeMap<String, Set<?>>();
+        detailMap.put("attachdoc", attachDocuments);
+        detailMap.put("attachform", attachForms);
+        detailMap.put("attachpic", attachPictures);
+        detailMap.put("attachelse", attachElses);
+        detailMap.put("other", otherDatas);
+        StringWriter writer = new StringWriter();
+        ObjectMapper mapper = new ObjectMapper();
+        try{
+            mapper.writeValue(writer, detailMap);
+        }catch (Exception e){
+            return internalServerError();
+        }
+        return ok(writer.toString());
+    }
 }
--- public/javascripts/homepage.js
+++ public/javascripts/homepage.js
@@ -1,7 +1,95 @@
-/**
- * Created with IntelliJ IDEA.
- * User: techzhou
- * Date: 12-9-18
- * Time: 下午4:28
- * To change this template use File | Settings | File Templates.
- */
+$(document).ready(
+    function () {
+        //init
+        $('#detail div').hide();
+        //search
+        $('#search_query').keyup(function (e) {
+                if (e.which == 13) {
+                    if($('#search_query').val().trim().length == 0)
+                        return;
+                    $('#detail div').hide();
+                    var progress = 0;
+                    $('#sidebar').html('<div class="progress progress-striped active"><div class="bar" style="width: 0%;"></div></div>');
+                    var wait = setInterval(function(){
+                        progress += 20;
+                        if(progress > 100){
+                            progress = 100;
+                            clearInterval(wait);
+                        }
+                        $('div.bar').css("width", progress + '%');
+                    }, 500);
+                    $.getJSON('/search/' + $('#search_query').val(), function(data) {
+                        items = "";
+                        $.each(data, function(key, val) {
+                            items += '<li class="nav-header">' + key + '</li>';
+                            $.each(val, function(index, element) {
+                                items += '<li data=\'' + JSON.stringify(element) + '\'><a href="#">' + element.name + '</a></li>';
+                            });
+                        });
+                        if(items.length == 0)
+                            $('#sidebar').text($('#search_query').val() + ' 查无结果');
+                        else{
+                            $('#sidebar').html(items);
+                        }
+                    });
+                }
+            }
+        );
+        //show detail
+        $('#sidebar li').live("click", function(){
+            $('#sidebar li').removeClass('active');
+            $(this).addClass("active");
+            var data = jQuery.parseJSON($(this).attr('data'));
+            $('#name').text(data.name);
+            var html = "";
+            html += "<span class='label'>资料编号："+ data.id +"</span><span class='label label-success'>资料类别："+ data.category +"</span>";
+            html += "<span class='label label-warning'>工作程度："+ data.jobProgress +"</span><span class='label label-info'>行政区划："+ data.location +"</span>";
+            var date = new Date();
+            date.setTime(data.finishDate);
+            html += "<span class='label label-important'>形成单位："+ data.department +"</span><span class='label label-inverse'>形成时间："+ date.format('yyyy-MM-dd') +"</span>";
+            $('#attribute').html(html);
+            getDetail(data.id);
+            $('#detail div').show();
+        });
+        //
+    }
+);
+
+Date.prototype.format = function(format) //author: meizz
+{
+    var o = {
+        "M+" : this.getMonth()+1, //month
+        "d+" : this.getDate(),    //day
+        "h+" : this.getHours(),   //hour
+        "m+" : this.getMinutes(), //minute
+        "s+" : this.getSeconds(), //second
+        "q+" : Math.floor((this.getMonth()+3)/3),  //quarter
+        "S" : this.getMilliseconds() //millisecond
+    }
+    if(/(y+)/.test(format)) format=format.replace(RegExp.$1,
+        (this.getFullYear()+"").substr(4 - RegExp.$1.length));
+    for(var k in o)if(new RegExp("("+ k +")").test(format))
+        format = format.replace(RegExp.$1,
+            RegExp.$1.length==1 ? o[k] :
+                ("00"+ o[k]).substr((""+ o[k]).length));
+    return format;
+}
+
+function getDetail(id) {
+    $('table tr').remove();
+    $.getJSON('/detail/' + id, function(data){
+        $.each(data, function(key, val) {
+            if(val.length == 0)
+                $('#' + key + ' table').append('<tr><td>查无结果</td></tr>');
+            else{
+                $.each(val, function(index, element) {
+                    $('#' + key + ' table').append('<tr onclick=readDoc(\"'+ key + '\",\"'+ element.id +'\")><td>' + element.id +'</td><td>' + element.name + '</td></tr>');
+                });
+            }
+        });
+    });
+}
+
+function readDoc(key,id) {
+    $.pageslide({ direction: 'left', href: '#reader' });
+}
